7ee48b7f3f90bd1a04a81bbe6e668c37583487d7
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-10-26 00:32:26 +0300

    updated categories to be fetched from db

--- src/app/_actions/Listing/updateListing.js
+++ src/app/_actions/Listing/updateListing.js
@@ -1,6 +1,10 @@
 'use server'
 import axios from "axios";
 export const updateListing = async (id, data) => {
+  try {
     const newlisting = await axios.put(`http://localhost:3000/listing/${id}`, data)
     return newlisting.data
+  } catch (err){
+    return err.message
   }
+}
