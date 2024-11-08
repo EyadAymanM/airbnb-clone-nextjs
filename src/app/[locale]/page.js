4c35ebe200290f8235b0da28371c92e87ef45a8e
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-08 16:14:31 +0200

    handeled and localized search

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -7,8 +7,14 @@ import CategoryList from "../_components/category";
 import Footer from "../_components/Footer/Footer";
 export default async function Home({ searchParams }) {
   const t = await getTranslations('HomePage');
-  
-  const listings = searchParams['category'] ? await fetchData(`listing/verified?category=${searchParams['category']}`) : await fetchData(`listing/verified`)
+
+  let listings = await fetchData(`listing/verified`)
+  if (searchParams['category']){
+    listings =  await fetchData(`listing/verified?category=${searchParams['category']}`)
+  }
+  if (searchParams['search']) {
+    listings = await fetchData(`listing/search?title=${searchParams['search']}`)
+  }
   // console.log('listings', listings);
   try {
     if (listings['message'])
@@ -30,10 +36,10 @@ export default async function Home({ searchParams }) {
       <NavBar show={true} position={"sticky"}/>
       <Container>
         <CategoryList />
-        <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-          {listings.map((listing, index) =>
+        <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 min-h-[550px]">
+          {listings.length > 0 ? listings.map((listing, index) =>
             <ListingCard className="mb-4" key={index} listing={listing} />
-          )}
+          ) : <h1 className="h-96 pt-40 font-airbnb text-2xl text-center col-span-4">{t('not-found-search')}</h1>}
         </div>
       </Container>
       <Footer position={"sticky"}/>
--- src/app/_components/Navbar/Search.jsx
+++ src/app/_components/Navbar/Search.jsx
@@ -1,8 +1,11 @@
 'use client';
 import { useRouter } from "@/i18n/routing";
+import { useLocale, useTranslations } from "next-intl";
 import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 const Searchbar = ({mobile}) => {
+  const t = useTranslations("search-listing")
+  const locale = useLocale()
   const router = useRouter()
   const [isFocused, setIsFocused] = useState(false)
   const [search, setSearch] = useState('')
@@ -20,13 +23,13 @@ const Searchbar = ({mobile}) => {
           className={`flex items-center rounded-full border-2 ${isFocused ? 'border-rose-500' : 'border-gray-300'
             } transition-colors duration-200`}
         >
-          <div onClick={handleSearch} className="absolute right-3 text-gray-400 hidden md:block cursor-pointer p-[5px] rounded-full hover:bg-gray-300">
+          <div onClick={handleSearch} className={`absolute ${locale == 'en'?'right-3':'left-3'} text-gray-400 hidden md:block cursor-pointer p-[5px] rounded-full hover:bg-gray-300`}>
             <BiSearch className={`h-5 w-5 rounded-full ${isFocused ? 'text-rose-500' : ''}`} />
           </div>
           <form onSubmit={handleSearch}>
           <input
             type="text"
-            placeholder="Search Listings..."
+            placeholder={t("placeholder")}
             className="w-full py-2 ps-6 text-sm text-gray-600 rounded-full border-none focus:outline-none focus:ring-0"
             onFocus={() => setIsFocused(true)}
             onBlur={() => setIsFocused(false)}
